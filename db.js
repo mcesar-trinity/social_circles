70668fb47501f78c2cf9f20e008b507c61017aa2
ShadowPikmin <jdcarter432@gmail.com>
2025-03-31 12:08:10 -0500

    Fixes database info

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Goten1!!',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME
 });
 
