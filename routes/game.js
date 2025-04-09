6b6e36e5c7358e1edbb4efbc60d02285139706db
ShadowPikmin <jdcarter432@gmail.com>
2025-04-09 08:48:12 -0500

    Commits current state of Game Page

--- routes/game.js
+++ routes/game.js
@@ -15,36 +15,37 @@ function capitalize(taskResult){
         }
 
         tasks[x] = splitter.join(' ');
-        console.log(tasks[x]);
     }
 
-    console.log(tasks);
     return tasks;
 }
 
 function getRandomTask(limit){
-    var tasks = new Array(5);
-    var counter = 0; 
-
-    while(counter != 5){
-        var idNum = Math.floor((Math.random() * parseInt(limit) + 1)); 
-        if(tasks.includes(idNum)){
-        }else{
-            tasks[counter] = idNum;
-            counter++;
-        }
+    var tasks = new Array(limit);
+    for(var x = 1; x <= limit; x++){
+        tasks[x-1] = x;
     }
 
-    return tasks;
+    for(var i = tasks.length - 1; i > 0; i--){
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = tasks[i];
+        tasks[i] = tasks[j];
+        tasks[j] = temp; 
+    }
+
+    var results = new Array(5);
+    for(var x = 0; x < 5; x++){
+        results[x] = tasks[x];
+    }
+
+    return results;
 }
 
 function createCharacterGroups(characterResult){
-    console.log(characterResult);
     splitter = Math.round(characterResult.length / 3);
     
     var groups = new Array(3).fill(0).map(() => new Array(splitter).fill(0));
-    console.log(groups);
-    
+
     for (var i = characterResult.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i+1));
         var temp = characterResult[i];
@@ -54,9 +55,7 @@ function createCharacterGroups(characterResult){
 
     for(var x = 0; x < 3; x++){
         for(var y = 0; y < splitter; y++){
-            console.log(splitter);
             groups[x][y] = characterResult[(splitter * x) + y];
-            console.log(groups);
         }
     }
     
@@ -70,23 +69,19 @@ router.get("/", (req, res) => {
         db.query(counterSQL, (counterErr, counterResult, fields) => {
             if(counterErr) throw counterErr; 
 
-            console.log(counterResult[0].tasksCount);
             var taskList = getRandomTask(counterResult[0].tasksCount);
             let taskSQL = `select name from tasks t where t.id in (` + db.escape(taskList) + `)`;
-            console.log(taskSQL);
 
             db.query(taskSQL, (taskErr, taskResult) => {
                 if(taskErr) throw taskErr;
                 
                 let characterSQL = `select c.name, uc.happiness_score from game_characters c join user_character_score uc
                  on c.id = uc.character_id where uc.user_id = ` + db.escape(req.session.user.id);
-                console.log(characterSQL);
 
                 db.query(characterSQL, (characterErr, characterResult) => {
                     if(characterErr) throw characterErr;
 
                     characterGroups = createCharacterGroups(characterResult);
-                    console.log(characterGroups);
                     res.render("game",{title: 'Social Circle Game', webTitle: 'Game Page', isUser:isUser, characterGroups:characterGroups, tasks:capitalize(taskResult)});
                 });
 
@@ -103,6 +98,12 @@ router.get("/", (req, res) => {
         isUser: isUser
     });
     }
+});
+
+
+router.post("/", (req, res) => {
+    console.log("Reached");
+    console.log(req.body);
 });
 
 module.exports = router;
