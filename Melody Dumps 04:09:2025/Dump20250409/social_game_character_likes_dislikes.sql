8214e68646c7ed2b9f1bc85799f7dd45d94748d8
MelodyAdeshina <144841762+MelodyAdeshina@users.noreply.github.com>
2025-04-09 19:55:53 -0500

    Add files via upload
    New Dump for Malea.

---
+++ Melody Dumps 04:09:2025/Dump20250409/social_game_character_likes_dislikes.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for macos15 (x86_64)
+--
+-- Host: 127.0.0.1    Database: social_game
+-- ------------------------------------------------------
+-- Server version	9.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `character_likes_dislikes`
+--
+
+DROP TABLE IF EXISTS `character_likes_dislikes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `character_likes_dislikes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `character_id` int DEFAULT NULL,
+  `category_id` int DEFAULT NULL,
+  `opinions` enum('like','dislike','love','hate') DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `character_id` (`character_id`),
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `character_likes_dislikes_ibfk_1` FOREIGN KEY (`character_id`) REFERENCES `game_characters` (`id`),
+  CONSTRAINT `character_likes_dislikes_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `task_categories` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `character_likes_dislikes`
+--
+
+LOCK TABLES `character_likes_dislikes` WRITE;
+/*!40000 ALTER TABLE `character_likes_dislikes` DISABLE KEYS */;
+INSERT INTO `character_likes_dislikes` VALUES (1,1,3,'love','2025-04-08 22:52:09','2025-04-08 22:52:09'),(2,1,1,'like','2025-04-08 22:52:09','2025-04-08 22:52:09'),(3,1,6,'dislike','2025-04-08 22:52:09','2025-04-08 22:52:09'),(4,1,5,'hate','2025-04-08 22:52:09','2025-04-08 22:52:09'),(5,2,5,'love','2025-04-08 22:52:09','2025-04-08 22:52:09'),(6,2,9,'like','2025-04-08 22:52:09','2025-04-08 22:52:09'),(7,2,2,'dislike','2025-04-08 22:52:09','2025-04-08 22:52:09'),(8,2,7,'hate','2025-04-08 22:52:09','2025-04-08 22:52:09'),(9,3,4,'love','2025-04-08 22:52:09','2025-04-08 22:52:09'),(10,3,7,'like','2025-04-08 22:52:09','2025-04-08 22:52:09'),(11,3,5,'dislike','2025-04-08 22:52:09','2025-04-08 22:52:09'),(12,3,3,'hate','2025-04-08 22:52:09','2025-04-08 22:52:09');
+/*!40000 ALTER TABLE `character_likes_dislikes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-09 19:50:06
