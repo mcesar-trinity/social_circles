012782986c39455a4814339f9939c88c895bdc24
ShadowPikmin <jdcarter432@gmail.com>
2025-04-11 00:41:37 -0500

    Fixes some mistakes in merge

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Blue2004',
-    database: 'social_game'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
      //// Use quotes or backticks if you include spaces
 });
 
