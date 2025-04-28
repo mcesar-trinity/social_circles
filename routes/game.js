b402260b84d5db8e2dc2bba6a83ac17cccf12aeb
ShadowPikmin <jdcarter432@gmail.com>
2025-04-27 23:21:02 -0500

    Commits merge

--- routes/game.js
+++ routes/game.js
@@ -240,7 +240,4 @@ router.post("/", (req, res) => {
 });
 
 // TESTING CODE: module.exports = {router, getRandomTask, createCharacterGroups, calculateHappiness};
-
-module.exports = router;
-//module.exports = {router, getRandomTask, createCharacterGroups, calculateHappiness};
 module.exports = router;
